dc4dbb409cc238198c3a0db5323358f49415fbda
TBHNhien <thaibaohaonhien1995@gmail.com>
2022-11-24 23:48:13 +0700

    24.11.2022

--- bai4.cpp
+++ bai4.cpp
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include<math.h>
 #include <string.h>
+#include <time.h>
 #define MAX 10
 int daDuyet[MAX];
 
@@ -127,13 +128,36 @@ void Prim (DOTHI g)
 int main()
 {
 	DOTHI g;
-	if (DocMaTranKe((char*)"text3.1.txt",g) == 1)
+	clock_t start , end;
+	double cpu_time_used;
+	int c=0;
+	
+	start = clock();
+//	for (int i=0;i<100000;i++){
+//		for(int j=0;j<10000;j++)
+//		{
+//			c++;
+//		}
+//	}
+	
+	if (DocMaTranKe((char*)"text2.txt",g) == 1)
 	{
 		printf("Da lay thong tin do thi tu file thanh cong.\n\n");
 		XuatMaTranKe(g);
 	}
-
+		
+	
+	
 	printf ("%d",soTPLT(g));
 	Prim(g);
+	
+	end=clock();
+	
+	cpu_time_used = (double)(end-start)/ CLOCKS_PER_SEC;
+	cpu_time_used *=1000;
+	
+	printf ("thoi gian chay : %f milisec",cpu_time_used);
+	
+	
 return 0;
 }
